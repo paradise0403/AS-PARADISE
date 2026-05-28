05c888f227859a91665bf759edc117d9c43e2746
PARADISE <ashish12canada@gmail.com>
2026-05-28 09:20:12 +0530

    Add files via upload

--- dist/ba_root/mods/chathandle/handlechat.py
+++ dist/ba_root/mods/chathandle/handlechat.py
@@ -12,28 +12,35 @@ from serverdata import serverdata
 from tools import logger, servercheck
 from features.discord_bot import chat_buffer, chat_lock, player_info
 
-# --- IMPORT QUIZ SYSTEM ---
+# --- IMPORT COIN + QUIZ SYSTEM ---
 try:
-    from ba_root.mods import question
+    from ba_root.mods import coin_system as coin
 except ImportError:
     try:
-        import question
-    except:
-        question = None
+        import coin_system as coin
+    except Exception:
+        coin = None
+
 
 def clean_bs_text(text: str) -> str:
     return ''.join(
-        ch for ch in text
+        ch for ch in str(text)
         if not (0xE000 <= ord(ch) <= 0xF8FF)
     )
 
+
 settings = setting.get_settings_data()
+
 
 def filter_chat_message(msg, client_id):
     if msg:
-        # --- QUIZ CHECK HOOK ---
-        if question:
-            question.handle_chat(msg, client_id)
+        # --- COIN QUIZ CHECK HOOK ---
+        if coin:
+            try:
+                if coin.handle_chat(msg, client_id):
+                    return None
+            except Exception as e:
+                print("Coin quiz hook error:", e)
 
         name = "Unknown"
         pbid = "unknown"
@@ -45,17 +52,12 @@ def filter_chat_message(msg, client_id):
                 v2 = i.get('display_string', 'unknown')
                 try:
                     name = i['players'][0]['name_full']
-                except:
+                except Exception:
                     name = "<in-lobby>"
                 break
 
         clean_name = clean_bs_text(name)
-        # clean_v2 = clean_bs_text(v2) # Not needed anymore
-        # clean_msg = clean_bs_text(msg) # Not needed anymore
 
-        # --- FIX: REMOVED MANUAL CHAT_BUFFER APPEND ---
-        # Ab yeh file chat_buffer mein kachra nahi bhejegi.
-        # Sirf player_info update karegi.
         with chat_lock:
             player_info[client_id] = {
                 "name": clean_name,
@@ -64,6 +66,7 @@ def filter_chat_message(msg, client_id):
             }
 
     now = datetime.now()
+
     if client_id == -1:
         if msg.startswith("/"):
             command_executor.execute(msg, client_id)
@@ -80,48 +83,79 @@ def filter_chat_message(msg, client_id):
             acid = i['account_id']
             try:
                 currentname = i['players'][0]['name_full']
-            except:
+            except Exception:
                 currentname = "<in-lobby>"
             displaystring = i['display_string']
 
     if acid:
         msg = chatfilter.filter(msg, acid, client_id)
     else:
-        bs.broadcastmessage("\ue00cFetching your account info , please wait\ue047",
-                            transient=True, clients=[client_id])
-        return
+        bs.broadcastmessage(
+            "\ue00cFetching your account info , please wait\ue047",
+            transient=True,
+            clients=[client_id]
+        )
+        return None
 
-    if msg == None:
-        return
+    if msg is None:
+        return None
 
-    # Logger abhi bhi file mein save karega, but double Discord nahi hoga
     logger.log(f'{acid}  |  {displaystring}| {currentname} | {msg}', "chat")
-    
+
     if msg.startswith("/"):
         msg = command_executor.execute(msg, client_id)
-        if msg == None:
-            return
+        if msg is None:
+            return None
 
     if msg in ["end", "dv", "nv", "sm"] and settings["allowVotes"]:
         votingmachine.vote(acid, client_id, msg)
 
     if acid in serverdata.clients and serverdata.clients[acid]["verified"]:
         if serverdata.muted:
-            bs.broadcastmessage("\ue041Server on mute\ue04d", transient=True, clients=[client_id])
-            return
-        elif acid in pdata.get_blacklist()["muted-ids"] and now < datetime.strptime(pdata.get_blacklist()["muted-ids"][acid]["till"], "%Y-%m-%d %H:%M:%S"):
-            bs.broadcastmessage("\ue04fYou are on mute\ue00c", transient=True, clients=[client_id])
+            bs.broadcastmessage(
+                "\ue041Server on mute\ue04d",
+                transient=True,
+                clients=[client_id]
+            )
             return None
-        elif servercheck.get_account_age(serverdata.clients[acid]["accountAge"]) < settings['minAgeToChatInHours']:
-            bs.broadcastmessage("\ue045New accounts not allowed to chat\ue04f", transient=True, clients=[client_id])
+
+        elif (
+            acid in pdata.get_blacklist()["muted-ids"]
+            and now < datetime.strptime(
+                pdata.get_blacklist()["muted-ids"][acid]["till"],
+                "%Y-%m-%d %H:%M:%S"
+            )
+        ):
+            bs.broadcastmessage(
+                "\ue04fYou are on mute\ue00c",
+                transient=True,
+                clients=[client_id]
+            )
             return None
+
+        elif servercheck.get_account_age(
+            serverdata.clients[acid]["accountAge"]
+        ) < settings['minAgeToChatInHours']:
+            bs.broadcastmessage(
+                "\ue045New accounts not allowed to chat\ue04f",
+                transient=True,
+                clients=[client_id]
+            )
+            return None
+
         else:
             if msg.startswith(",") and settings["allowTeamChat"]:
                 return command_executor.QuickAccess(msg, client_id)
+
             if msg.startswith(".") and settings["allowInGameChat"]:
                 return command_executor.QuickAccess(msg, client_id)
+
             return msg
+
     else:
-        bs.broadcastmessage("\ue020Fetching your account info\ue046", transient=True, clients=[client_id])
+        bs.broadcastmessage(
+            "\ue020Fetching your account info\ue046",
+            transient=True,
+            clients=[client_id]
+        )
         return None
- 
